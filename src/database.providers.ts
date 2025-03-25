6d87e85f7cbfc779ae3519c269b5dad0034ac309
PosikBoy <z.kalinichenko4@gmail.com>
2025-03-25 15:48:48 +0300

    обновление рут переменных

--- src/database.providers.ts
+++ src/database.providers.ts
@@ -1,15 +1,15 @@
-import { Sequelize } from 'sequelize-typescript';
-import { Courier } from './couriers/couriers.model';
-import { Client } from './clients/clients.model';
-import { Address } from './addresses/addresses.model';
-import { Order } from './orders/orders.model';
-import { OrderStatus } from './orders/order-statuses.model';
-import { OrderAction } from './order-actions/order-actions.model';
-import { User } from './users/user.model';
-import { Chats } from './chats/chat/chat.model';
-import { Messages } from './chats/messages/messages.model';
-import { ChatParticipants } from './chats/chatParticipants/chat-participants.model';
-import { Files } from './files/files.model';
+import { Sequelize } from 'sequelize-typescript'
+import { Address } from './addresses/addresses.model'
+import { Chats } from './chats/chat/chat.model'
+import { ChatParticipants } from './chats/chatParticipants/chat-participants.model'
+import { Messages } from './chats/messages/messages.model'
+import { Client } from './clients/clients.model'
+import { Courier } from './couriers/couriers.model'
+import { Files } from './files/files.model'
+import { OrderAction } from './order-actions/order-actions.model'
+import { OrderStatus } from './orders/order-statuses.model'
+import { Order } from './orders/orders.model'
+import { User } from './users/user.model'
 
 export const databaseProviders = [
   {
@@ -18,10 +18,10 @@ export const databaseProviders = [
       const sequelize = new Sequelize({
         dialect: 'mysql',
         host: 'localhost',
-        port: Number(process.env.DB_PORT),
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
+        port: Number(process.env.MYSQL_PORT),
+        username: process.env.MYSQL_USER,
+        password: process.env.MYSQL_ROOT_PASSWORD,
+        database: process.env.MYSQL_DATABASE,
       });
       sequelize.addModels([
         Courier,
