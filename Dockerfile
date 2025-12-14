1dea8d7cac6f4bd19ed03257eac08b3e0980007a
PosikBoy <z.kalinichenko4@gmail.com>
2025-12-14 19:36:21 +0300

    Updated docker configuration

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:23-alpine3.20
+FROM node:25-alpine3.22
 WORKDIR /app
 
 COPY package*.json .
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,20 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AddressesModule } from './addresses/addresses.module';
-import { AuthModule } from './auth/auth.module';
-import { ChatModule } from './chats/chat/chat.module';
-import { MessageModule } from './chats/messages/messages.module';
-import { ClientsModule } from './clients/clients.module';
-import { CouriersModule } from './couriers/couriers.module';
-import { databaseProviders } from './database.providers';
-import { FilesModule } from './files/files.module';
-import { GeodataModule } from './geodata/geodata.module';
-import { OrderActionsModule } from './order-actions/order-actions.module';
-import { OrdersModule } from './orders/orders.module';
-import { PriceModule } from './price/price.module';
-import { SuggestionModule } from './suggestions/suggestions.module';
-import { TelegramNotifyModule } from './telegram-notify/telegram-notify.module';
-import { TokensModule } from './tokens/tokens.module';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { AddressesModule } from "./addresses/addresses.module";
+import { AuthModule } from "./auth/auth.module";
+import { ChatModule } from "./chats/chat/chat.module";
+import { MessageModule } from "./chats/messages/messages.module";
+import { ClientsModule } from "./clients/clients.module";
+import { CouriersModule } from "./couriers/couriers.module";
+import { databaseProviders } from "./database.providers";
+import { FilesModule } from "./files/files.module";
+import { GeodataModule } from "./geodata/geodata.module";
+import { OrderActionsModule } from "./order-actions/order-actions.module";
+import { OrdersModule } from "./orders/orders.module";
+import { PriceModule } from "./price/price.module";
+import { SuggestionModule } from "./suggestions/suggestions.module";
+import { TelegramNotifyModule } from "./telegram-notify/telegram-notify.module";
+import { TokensModule } from "./tokens/tokens.module";
 
 @Module({
   providers: [...databaseProviders],
@@ -22,7 +22,7 @@ import { TokensModule } from './tokens/tokens.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: `.${process.env.NODE_ENV}.env`,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
     CouriersModule,
     OrdersModule,
