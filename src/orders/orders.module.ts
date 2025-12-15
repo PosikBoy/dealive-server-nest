855f3d2013dfb3ae94cd1654ab0f0f2e98bcd21f
PosikBoy <z.kalinichenko4@gmail.com>
2025-12-15 04:41:41 +0300

    Added import

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,15 +1,16 @@
-import { Module } from '@nestjs/common';
-import { OrdersService } from './orders.service';
-import { ordersRepository } from './orders.providers';
-import { OrdersController } from './orders.controller';
-import { addressesRepository } from '@/addresses/addresses.providers';
-import { TokensModule } from '@/tokens/tokens.module';
-import { ClientsModule } from '@/clients/clients.module';
-import { OrderActionsModule } from '@/order-actions/order-actions.module';
-import { UserModule } from '@/users/user.module';
-import { TelegramNotifyModule } from '@/telegram-notify/telegram-notify.module';
-import { GeodataModule } from '@/geodata/geodata.module';
-import { ThrottlerModule } from '@nestjs/throttler';
+import { addressesRepository } from "@/addresses/addresses.providers";
+import { ClientsModule } from "@/clients/clients.module";
+import { GeodataModule } from "@/geodata/geodata.module";
+import { OrderActionsModule } from "@/order-actions/order-actions.module";
+import { PriceModule } from "@/price/price.module";
+import { TelegramNotifyModule } from "@/telegram-notify/telegram-notify.module";
+import { TokensModule } from "@/tokens/tokens.module";
+import { UserModule } from "@/users/user.module";
+import { Module } from "@nestjs/common";
+import { ThrottlerModule } from "@nestjs/throttler";
+import { OrdersController } from "./orders.controller";
+import { ordersRepository } from "./orders.providers";
+import { OrdersService } from "./orders.service";
 
 @Module({
   providers: [OrdersService, ...ordersRepository, ...addressesRepository],
@@ -21,6 +22,7 @@ import { ThrottlerModule } from '@nestjs/throttler';
     UserModule,
     TelegramNotifyModule,
     GeodataModule,
+    PriceModule,
     ThrottlerModule.forRoot([
       {
         ttl: 60000,
